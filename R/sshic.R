d3134ae76868c0e0fb47eacfc764e9eb5e377e5d
SpatLyu <lyu.geosocial@gmail.com>
2024-12-01 08:36:22 +0800

    add sshic

--- R/sshic.R
+++ R/sshic.R
@@ -1,6 +1,21 @@
-sshic = \(d, s,
-          permutation = 999,
-          bin_method = "SquareRoot",
-          seed = 123456789) {
-
+#' Measurement of Spatial Stratified Heterogeneity Based on Information Consistency for Continuous Variables
+#'
+#' @param d The target variable.
+#' @param s The stratification.
+#' @param seed (optional) Random number seed, default is `42`.
+#' @param permutation_number (optional) Number of Random Permutations, default is `999`.
+#' @param bin_method (optional) Histogram binning method for probability density estimation, default is
+#' `FreedmanDiaconis`.
+#'
+#' @return A two-element numerical vector.
+#' @export
+#'
+#' @examples
+sshic = \(d, s, seed = 42,
+          permutation_number = 999,
+          bin_method = "FreedmanDiaconis") {
+  s = as.integer(as.factor(s))
+  res = IC_SSHICM(d,s,seed,permutation_number,bin_method)
+  names(res) = c("IC","PV")
+  return(res)
 }
