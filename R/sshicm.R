4959c2377d227c6f6940f1d60ca79bff623ca89f
SpatLyu <lyu.geosocial@gmail.com>
2024-12-01 14:20:14 +0800

    add sshicm

---
+++ R/sshicm.R
@@ -0,0 +1,42 @@
+#' Information Consistency-Based Measures for Spatial Stratified Heterogeneity
+#'
+#' @param formula A formula.
+#' @param data A `data.frame`, `tibble` or `sf` object of observation data.
+#' @param type (optional) Measure type, default is `IC`.
+#' @param seed (optional) Random number seed, default is `42`.
+#' @param permutation_number (optional) Number of Random Permutations, default is `999`.
+#' @param bin_method (optional) Histogram binning method for probability density estimation, default is
+#' `Sturges`.
+#'
+#' @return A `tibble`.
+#' @export
+#'
+#' @examples
+#' baltim = sf::read_sf(system.file("extdata/baltim.gpkg",package = "sshicm"))
+#' sshicm(PRICE ~ .,baltim,type = "Ic")
+#' cinc = sf::read_sf(system.file("extdata/cinc.gpkg",package = "sshicm"))
+#' sshicm(THEFT_D ~ .,cinc,type = "IN")
+#'
+sshicm = \(formula, data, type = 'IC', seed = 42,
+           permutation_number = 999, bin_method = "Sturges"){
+  formulavar = sdsfun::formula_varname(formula,data)
+  yvec = data[,formulavar[[1]],drop = TRUE]
+
+  if (inherits(data,"sf")){
+    data = sf::st_drop_geometry(data)
+  }
+  xtbl = dplyr::select(data,dplyr::all_of(formulavar[[2]]))
+
+  if (type == "IC"){
+    res = purrr::map_dfr(xtbl,
+                         \(.x) sshic(yvec,.x,seed,
+                                     permutation_number,
+                                     bin_method))
+  } else {
+    res = purrr::map_dfr(xtbl,
+                         \(.x) sshin(yvec,.x,seed,
+                                     permutation_number))
+  }
+
+  return(res)
+}
